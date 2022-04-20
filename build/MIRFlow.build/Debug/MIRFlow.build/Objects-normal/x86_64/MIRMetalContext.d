494216a904983039fea9d21f58e581d59d7622d4
mirrorsysu <844912709@qq.com>
2022-04-20 21:19:36 +0800

    .

--- build/MIRFlow.build/Debug/MIRFlow.build/Objects-normal/x86_64/MIRMetalContext.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/mirror/Desktop/MIRFlow/MIRFlow/misc/MIRMetal/MIRMetalContext.m \
-  /Users/mirror/Desktop/MIRFlow/MIRFlow/misc/MIRMetal/MIRMetalContext.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.0.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.0.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/mirror/Desktop/MIRFlow/MIRFlow/misc/MIRMetal/MIRMetalShaderTypes.h
